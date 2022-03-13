a2c002b140a726b63772bd52889a0c4e8759a092
Johnny Almonte <johnny243@users.noreply.github.com>
2022-03-13 01:32:46 -0400

    feat(wip): TaskEditor component

--- src/components/Editor.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Editor from './Editor';
-
-test('renders Standard Notes documentation link', () => {
-  render(<Editor />);
-  const linkElement = screen.getByText(/Standard Notes documentation/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import EditorKit, { EditorKitDelegate } from '@standardnotes/editor-kit';
 
-import Editor from './components/Editor';
 import './stylesheets/main.scss';
+
+type Props = {};
+type State = {
+  editable: boolean;
+  spellcheck: boolean;
+  isLoading: boolean;
+};
+
+class TaskEditor extends Component<Props, State> {
+  private editorKit?: EditorKit;
+  private prevText: string = '';
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      editable: true,
+      spellcheck: true,
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.configureEditorKit();
+  }
+
+  private configureEditorKit() {
+    const editorKitDelegate: EditorKitDelegate = {
+      setEditorRawText: (text: string) => {
+        this.prevText = text.trim();
+
+        this.setState({
+          isLoading: false,
+        });
+      },
+      generateCustomPreview: (text: string) => {
+        return {
+          plain: text,
+        };
+      },
+      onNoteValueChange: async (note: any) => {
+        const editable =
+          !note.content.appData['org.standardnotes.sn'].locked ?? true;
+        const spellcheck = note.content.spellcheck;
+
+        this.setState({
+          editable,
+          spellcheck,
+        });
+      },
+      onNoteLockToggle: (locked: boolean) => {
+        const editable = !locked;
+
+        this.setState({
+          editable,
+        });
+      },
+    };
+
+    this.editorKit = new EditorKit(editorKitDelegate, {
+      mode: 'json',
+      supportsFileSafe: false,
+    });
+  }
+
+  private onTextChange(text: string) {
+    if (this.prevText === text.trim()) {
+      return;
+    }
+
+    this.prevText = text.trim();
+    this.editorKit!.onEditorValueChanged(text);
+  }
+
+  render() {
+    const { isLoading } = this.state;
+
+    if (isLoading) {
+      return null;
+    }
+
+    return <></>;
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <Editor />
+    <TaskEditor />
   </React.StrictMode>,
   document.getElementById('root')
 );
