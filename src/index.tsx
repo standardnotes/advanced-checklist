9779bce1691ce673a37a9a56e6d8e67293ff09d8
Johnny Almonte <johnny243@users.noreply.github.com>
2022-03-14 16:07:37 -0400

    feat: TaskEditor

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,39 @@
+import './stylesheets/main.scss';
+
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import EditorKit, { EditorKitDelegate } from '@standardnotes/editor-kit';
 
-import './stylesheets/main.scss';
+import TaskRow from './components/TaskRow';
+import CreateTask from './components/CreateTask';
+
+import Task from './models/Task';
+import TaskList from './models/TaskList';
+
+import EditorKit, { EditorKitDelegate } from '@standardnotes/editor-kit';
 
 type Props = {};
 type State = {
   editable: boolean;
-  spellcheck: boolean;
-  isLoading: boolean;
+  spellCheckEnabled: boolean;
+  taskDraft: string;
+  openTasks: Task[];
+  completedTasks: Task[];
 };
 
 class TaskEditor extends Component<Props, State> {
   private editorKit?: EditorKit;
-  private prevText: string = '';
+  private taskList?: TaskList;
+  private note?: any;
 
   constructor(props: Props) {
     super(props);
 
     this.state = {
       editable: true,
-      spellcheck: true,
-      isLoading: true,
+      spellCheckEnabled: true,
+      taskDraft: '',
+      openTasks: [],
+      completedTasks: [],
     };
   }
 
@@ -29,28 +41,37 @@ class TaskEditor extends Component<Props, State> {
     this.configureEditorKit();
   }
 
+  get ready(): boolean {
+    return !!this.editorKit;
+  }
+
+  get showTutorial(): boolean {
+    return (
+      this.editorKit!.getComponentDataValueForKey('showTutorial') === undefined
+    );
+  }
+
+  get isRunningOnMobile(): boolean {
+    return this.editorKit!.isRunningInMobileApplication();
+  }
+
   private configureEditorKit() {
     const editorKitDelegate: EditorKitDelegate = {
-      setEditorRawText: (text: string) => {
-        this.prevText = text.trim();
-
-        this.setState({
-          isLoading: false,
-        });
-      },
-      generateCustomPreview: (text: string) => {
-        return {
-          plain: text,
-        };
+      setEditorRawText: (rawString: string) => {
+        this.taskList = new TaskList(rawString);
+        this.updateTasks();
       },
       onNoteValueChange: async (note: any) => {
+        this.note = note;
+
         const editable =
           !note.content.appData['org.standardnotes.sn'].locked ?? true;
-        const spellcheck = note.content.spellcheck;
+        const spellCheckEnabled = note.content.spellcheck;
 
         this.setState({
           editable,
-          spellcheck,
+          spellCheckEnabled,
+          taskDraft: note.content.taskDraft,
         });
       },
       onNoteLockToggle: (locked: boolean) => {
@@ -63,28 +84,149 @@ class TaskEditor extends Component<Props, State> {
     };
 
     this.editorKit = new EditorKit(editorKitDelegate, {
-      mode: 'json',
+      mode: 'plaintext',
       supportsFileSafe: false,
     });
   }
 
-  private onTextChange(text: string) {
-    if (this.prevText === text.trim()) {
-      return;
+  updateTasks() {
+    const { openTasks, completedTasks } = this.taskList!.splitTasks();
+
+    this.setState({
+      openTasks,
+      completedTasks,
+    });
+  }
+
+  deleteTask(task: Task) {
+    this.taskList!.deleteTask(task);
+    this.updateTasks();
+    this.save();
+  }
+
+  toggleTaskStatus = (task: Task) => {
+    task.toggleStatus();
+
+    if (!task.completed) {
+      this.taskList!.moveTaskToTop(task);
     }
 
-    this.prevText = text.trim();
-    this.editorKit!.onEditorValueChanged(text);
+    setTimeout(() => {
+      // Allow UI to show checkmark before transferring to other list.
+      this.updateTasks();
+      this.save();
+    }, 300);
+  };
+
+  private createTask = (rawString: string) => {
+    const task = this.taskList!.createTask(rawString);
+    this.taskList!.addTask(task);
+    this.updateTaskDraft('');
+    this.updateTasks();
+    this.save();
+  };
+
+  private updateTaskDraft = (rawString: string, save: boolean = false) => {
+    this.setState({
+      taskDraft: rawString,
+    });
+    save && this.save();
+  };
+
+  private onReOpenCompleted = () => {
+    if (window.confirm('Are you sure you want to reopen completed tasks?')) {
+      this.taskList!.reOpenCompleted();
+      this.updateTasks();
+      this.save();
+    }
+  };
+
+  private onDeleteCompleted = () => {
+    if (window.confirm('Are you sure you want to delete completed tasks?')) {
+      this.taskList!.deleteCompleted();
+      this.updateTasks();
+      this.save();
+    }
+  };
+
+  private taskRowForTask(task: Task, index: number) {
+    const { spellCheckEnabled } = this.state;
+    return (
+      <TaskRow
+        deleteTask={this.deleteTask}
+        handleCheckboxChange={this.toggleTaskStatus}
+        handleTextChange={() => this.save()}
+        key={`${index}-${task.rawString}`}
+        spellCheckEnabled={spellCheckEnabled}
+        task={task}
+      />
+    );
+  }
+
+  private save() {
+    const note = this.note;
+
+    this.editorKit!.saveItemWithPresave(note, () => {
+      note.content.text = this.taskList!.getDataString();
+      note.content.taskDraft = this.state.taskDraft;
+      note.content.preview_html = this.taskList!.buildHtmlPreview();
+      note.content.preview_plain = this.taskList!.buildPlainPreview();
+    });
+
+    if (this.showTutorial) {
+      this.editorKit!.setComponentDataValueForKey('showTutorial', false);
+    }
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { spellCheckEnabled, openTasks, completedTasks, taskDraft } =
+      this.state;
 
-    if (isLoading) {
-      return null;
-    }
+    return (
+      <>
+        {this.ready && (
+          <div className="task-input">
+            <CreateTask
+              isMobile={this.isRunningOnMobile}
+              onSubmit={this.createTask}
+              onUpdate={this.updateTaskDraft}
+              showTutorial={this.showTutorial}
+              spellCheckEnabled={spellCheckEnabled}
+              taskDraft={taskDraft}
+            />
+          </div>
+        )}
 
-    return <></>;
+        <div className="task-section">
+          <h3>Open Tasks</h3>
+          <div id="completed-tasks">
+            {openTasks.map((task, index) => {
+              return this.taskRowForTask(task, index);
+            })}
+          </div>
+        </div>
+
+        <div className="task-section">
+          <h3>Completed Tasks</h3>
+          <div id="completed-tasks">
+            {completedTasks.map((task, index) => {
+              return this.taskRowForTask(task, index);
+            })}
+          </div>
+
+          {completedTasks.length > 0 && (
+            <div>
+              <button className="clear-button" onClick={this.onReOpenCompleted}>
+                Reopen Completed
+              </button>
+              <button className="clear-button" onClick={this.onDeleteCompleted}>
+                Delete Completed
+              </button>
+            </div>
+          )}
+        </div>
+      </>
+    );
   }
 }
 
