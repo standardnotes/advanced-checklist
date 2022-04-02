1113b6fa9c6f2a0523bfd91e4340b368f61c7d43
Johnny Almonte <johnny243@users.noreply.github.com>
2022-04-02 19:37:42 -0400

    feat(wip): integrate EditorKit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import './stylesheets/main.scss';
 
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 import CreateGroup from './features/tasks/CreateGroup';
@@ -16,28 +16,82 @@ import {
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { tasksLoaded } from './features/tasks/tasks-slice';
-import mockData from './mockData';
+import EditorKit, { EditorKitDelegate } from '@standardnotes/editor-kit';
 
 const TaskEditor: React.FC = () => {
+  const currentNote = useRef<any>();
+  const editorKit = useRef<EditorKit>();
+
   const groupedTasks = useAppSelector((state) => state.tasks.storage);
   const canEdit = useAppSelector((state) => state.settings.canEdit);
 
   const dispatch = useAppDispatch();
 
-  const configureEditorKit = useCallback(() => {
-    // EditorKit setup goes here...
-    dispatch(setCanEdit(true));
-    dispatch(setSpellCheckerEnabled(true));
-    dispatch(setIsMobile(false));
-    dispatch(setShowTutorial(true));
+  function showTutorial(): boolean {
+    return true;
+    // return (
+    //   editorKit.current!.getComponentDataValueForKey('showTutorial') === undefined
+    // );
+  }
 
-    const serialized = JSON.stringify(mockData);
-    dispatch(tasksLoaded(serialized));
+  function isRunningOnMobile(): boolean {
+    return false;
+    // return editorKit.current!.isRunningInMobileApplication();
+  }
+
+  const saveNote = useCallback(() => {
+    const note = currentNote.current;
+    if (!note) {
+      return;
+    }
+
+    editorKit.current!.saveItemWithPresave(note, () => {
+      const groupedTasks = store.getState().tasks.storage;
+      note.content.text = JSON.stringify(groupedTasks);
+      note.content.preview_html = '<span>WIP</span>';
+      note.content.preview_plain = 'WIP';
+    });
+
+    if (showTutorial()) {
+      // editorKit.current!.setComponentDataValueForKey('showTutorial', false);
+    }
+  }, []);
+
+  const configureEditorKit = useCallback(() => {
+    const editorKitDelegate: EditorKitDelegate = {
+      setEditorRawText: (rawString: string) => {
+        dispatch(tasksLoaded(rawString));
+      },
+      onNoteValueChange: async (note: any) => {
+        currentNote.current = note;
+
+        const editable =
+          !note.content.appData['org.standardnotes.sn'].locked ?? true;
+        const spellCheckEnabled = note.content.spellcheck;
+
+        dispatch(setCanEdit(editable));
+        dispatch(setSpellCheckerEnabled(spellCheckEnabled));
+        dispatch(setIsMobile(isRunningOnMobile()));
+        dispatch(setShowTutorial(showTutorial()));
+      },
+      onNoteLockToggle: (locked: boolean) => {
+        dispatch(setCanEdit(!locked));
+      },
+    };
+
+    editorKit.current = new EditorKit(editorKitDelegate, {
+      mode: 'json',
+      supportsFileSafe: false,
+    });
   }, [dispatch]);
 
   useEffect(() => {
     configureEditorKit();
   }, [configureEditorKit]);
+
+  useEffect(() => {
+    return store.subscribe(saveNote);
+  }, [saveNote]);
 
   return (
     <>
