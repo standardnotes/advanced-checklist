4deed08304dc2476c44ddca0a9b0a32764c84d4f
Johnny Almonte <johnny243@users.noreply.github.com>
2022-05-03 17:52:07 -0400

    fix: circular progress bar background color

--- src/common/components/CircularProgressBar.tsx
+++ src/common/components/CircularProgressBar.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const ProgressBarBackground = styled.circle`
   fill: none;
-  stroke: var(--sn-stylekit-contrast-background-color);
+  stroke: var(--sn-stylekit-contrast-border-color);
 `
 
 const ProgressBarStroke = styled.circle`
