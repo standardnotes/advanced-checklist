9a2a13234900fce88c602ab8449bbae5c89772fd
Johnny Almonte <johnny243@users.noreply.github.com>
2022-05-13 20:24:10 -0400

    fix: remove Horizontal Progress Bar

--- src/common/components/HorizontalProgressBar.tsx
+++
@@ -1,8 +0,0 @@
-type ProgressBarProps = {
-  value: number
-  max: number
-}
-
-export const ProgressBar: React.FC<ProgressBarProps> = ({ ...props }) => {
-  return <progress style={{ marginTop: '10px', width: '100%' }} {...props} />
-}
