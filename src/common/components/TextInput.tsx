48d044aa0cfd8ae6d7b80457de9d50c0d9968eb1
Johnny Almonte <johnny243@users.noreply.github.com>
2022-05-18 11:55:49 -0400

    fix: input text color

--- src/common/components/TextInput.tsx
+++ src/common/components/TextInput.tsx
@@ -8,7 +8,6 @@ type StyledInputProps = {
 const StyledInput = styled.input<StyledInputProps>`
   background-color: unset;
   border: none;
-  color: var(--sn-stylekit-paragraph-text-color);
   font-size: ${({ textSize }) =>
     textSize === 'big' ? '1.125rem' : 'var(--sn-stylekit-font-size-h3)'};
   font-weight: ${({ textSize }) => (textSize === 'big' ? '500' : '400')};
